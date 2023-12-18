cf5450a277d3b1145b0ab2ae0f1811017744fe93
AHMED <hossainahmad76@gmail.com>
2023-12-18 23:27:23 +0600

    Google Map

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const GoogleMapsApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GoogleMapsApp extends StatelessWidget {
+  const GoogleMapsApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Home'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+            zoom: 19,
+            target: LatLng(23.792265005916146, 90.40561775869223),
+            bearing: 0,
+            tilt: 5),
+        onTap: (LatLng position) {
+          print(position);
+        },
+        onLongPress: (LatLng latLng) {
+          print('On long press at $latLng');
+        },
+        onCameraMove: (cameraPosition) {
+          print(cameraPosition);
+        },
+        zoomControlsEnabled: false,
+        zoomGesturesEnabled: false,
+        compassEnabled: false,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
